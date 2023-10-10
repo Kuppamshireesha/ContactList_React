e136583f0f6fd181ec4b58011e9e64cc4e2f765f
shireesha <kuppamshireesha@gmail.com>
2023-10-10 16:34:10 +0530

    first commit

---
+++ src/contacts.js
@@ -0,0 +1,39 @@
+const contacts = [
+  {
+      name: "Allu Arjun",
+      imgURL:
+        "https://wallpapers.com/images/featured/allu-arjun-hd-tbido00kjnbzoltw.jpg",
+      phone: "+918 372 574",
+      email: "Arjun@allu.com"
+  },
+  {
+    name: "Alia Bhatt",
+    imgURL:
+      "https://img.indiaforums.com/person/480x360/1/0973-alia-bhatt.jpg?c=7tC4D4",
+    phone: "+123 456 789",
+    email: "aiabhatt@alia.com"
+  },
+  {
+      name: "Kyile Jenner",
+      imgURL:
+        "https://media.allure.com/photos/645905e7cda1d353a84dfadd/1:1/w_2460,h_2460,c_limit/kylie%20jenner%20bleached%20eyebrows.jpg",
+      phone: "+918 372 574",
+      email: "Kyile@jenner.com"
+  },
+  {
+    name: "Shah Rukh Khan",
+    imgURL:
+      "https://img.indiaforums.com/person/480x360/0/0002-shah-rukh-khan.jpg?c=4sR1C4",
+    phone: "+987 654 321",
+    email: "shahrukh@khan.com"
+  },
+  {
+    name: "Ranbir Kapoor",
+    imgURL:
+      "https://img.indiaforums.com/person/480x360/0/1033-ranbir-kapoor.jpg?c=1vF2D7",
+    phone: "+918 372 574",
+    email: "Ranbir@kapoor.com"
+  }
+];
+
+export default contacts;
